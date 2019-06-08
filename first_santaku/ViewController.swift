16ce09fbc4f8cb6d695734fbd1df749a4da06698
AtoraYamada <atora2018@gmail.com>
2019-06-08 13:33:38 +0900

    Initial Commit

---
+++ first_santaku/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  first_santaku
+//
+//  Created by 山田 亜虎 on 2019/06/08.
+//  Copyright © 2019 山田 亜虎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
