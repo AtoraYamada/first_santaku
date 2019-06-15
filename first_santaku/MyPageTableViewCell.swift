1648d3ad77a7b24b5fff333c97f3aa4fe42ed455
AtoraYamada <atora2018@gmail.com>
2019-06-15 17:30:49 +0900

    create MyPageTableViewCell

---
+++ first_santaku/MyPageTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyPageTableViewCell.swift
+//  first_santaku
+//
+//  Created by 山田 亜虎 on 2019/06/15.
+//  Copyright © 2019 山田 亜虎. All rights reserved.
+//
+
+import UIKit
+
+class MyPageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
