551ab67a9ba43b830fc50032d811f186a48f8257
AtoraYamada <atora2018@gmail.com>
2019-06-17 21:16:34 +0900

    create splash storyboard and controller

---
+++ first_santaku/SplashViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SplashViewController.swift
+//  first_santaku
+//
+//  Created by 山田 亜虎 on 2019/06/17.
+//  Copyright © 2019 山田 亜虎. All rights reserved.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
