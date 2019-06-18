bfb1d52c10a8a83f499c42320b8fd8f24ebc5d22
AtoraYamada <atora2018@gmail.com>
2019-06-18 12:03:24 +0900

    set action for buttons in Search

--- first_santaku/SearchViewController.swift
+++ first_santaku/SearchViewController.swift
@@ -25,5 +25,57 @@ class SearchViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    var searchTags = Array<String>()
+    @IBAction func htmlButton(_ sender: CheckBox) {
+        if !sender.isChecked == true {
+            searchTags.append("HTML")
+        }else{
+            searchTags.remove(value: "HTML")
+        }
+        
+    }
+    
+    @IBAction func cssButton(_ sender: CheckBox) {
+        if !sender.isChecked == true {
+            searchTags.append("CSS")
+        }else{
+            searchTags.remove(value: "CSS")
+        }
+        
+    }
+    @IBAction func rubyButton(_ sender: CheckBox) {
+        if !sender.isChecked == true {
+            searchTags.append("Ruby")
+        }else{
+            searchTags.remove(value: "Ruby")
+        }
+    }
+    @IBAction func railsButton(_ sender: CheckBox) {
+        if !sender.isChecked == true {
+            searchTags.append("Rails")
+        }else{
+            searchTags.remove(value: "Rails")
+        }
+    }
+    @IBAction func jsButton(_ sender: CheckBox) {
+        if !sender.isChecked == true {
+            searchTags.append("JavaScript")
+        }else{
+            searchTags.remove(value: "JavaScript")
+        }
+    }
+    @IBAction func jqueryButton(_ sender: CheckBox) {
+        if !sender.isChecked == true {
+            searchTags.append("jQuery")
+        }else{
+            searchTags.remove(value: "jQuery")
+        }
+    }
+    @IBAction func othersButton(_ sender: CheckBox) {
+        if !sender.isChecked == true {
+            searchTags.append("Others")
+        }else{
+            searchTags.remove(value: "Others")
+        }
+    }
 }
