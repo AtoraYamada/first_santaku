21da19d6c585a7b88cc766bcd9da54ca06b56711
AtoraYamada <atora2018@gmail.com>
2019-06-18 10:12:31 +0900

    create seacrh view and controller

---
+++ first_santaku/SearchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SearchViewController.swift
+//  first_santaku
+//
+//  Created by 山田 亜虎 on 2019/06/18.
+//  Copyright © 2019 山田 亜虎. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
